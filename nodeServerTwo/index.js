d88a39fbd394c643b0aa244ef3bf308c2b3d6a35
S.M.Kashif <kashi_rock82@yahoo.com>
2021-03-01 22:09:55 +0500

    node server two

---
+++ nodeServerTwo/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const { json } = require('body-parser');
+const port = process.env.PORT || 8081;
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+app.get('/demo',  function(req, res){
+    return res.json({
+        message: 'hello world'
+    });
+});
+
+app.listen(port,()=>{
+    console.log("Connection has been published on port " +port);
+})
