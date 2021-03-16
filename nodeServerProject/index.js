4aeea49eaaae5e75fe13ef737ad50472c14d2a02
S.M.Kashif <kashi_rock82@yahoo.com>
2021-03-16 23:13:57 +0500

    changes

--- nodeServerProject/index.js
+++ nodeServerProject/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
 
-app.use(express.static('angularProject'));
+app.use(express.static('angularServerTestApp'));
 
 app.listen(port,()=>{
     console.log("Connection has been published on port " +port);
--- nodeServerTwo/index.js
+++ nodeServerTwo/index.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
-const { json } = require('body-parser');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 const port = process.env.PORT || 8081;
 
-app.use(bodyparser.urlencoded({extended: false}));
-app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
-app.get('/demo',  function(req, res){
+app.use(cookieParser());
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000,
+        token:"asassdffdgdfghsdfhfd"
+    }
+}));
+
+var sessionChecker = (req, res, next) => {
+    console.log(req.session , req.cookies);
+    if (req.session.user && req.cookies.user_sid) {
+        res.json({
+            status: 0,
+            msg: "session has been expired"
+        })
+    } else {
+        next();
+    }    
+};
+
+app.get('/demo', sessionChecker, function(req, res){
     return res.json({
         message: 'hello world',
-        dateTime: Date.now()
+        dateTime: Date.now(),
+        session: req.session,
+        cookie: req.cookies
     });
 });
 
